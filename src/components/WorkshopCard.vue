96394cf891060a59922bcd7c0ad4d7d527233234
Tom Bierschenk <bierscht@hu-berlin.de>
2020-06-21 11:41:08 +0200

    refactoring

--- src/components/WorkshopCard.vue
+++ src/components/WorkshopCard.vue
@@ -1,33 +1,13 @@
 <template>
-  <!--
-  <Card class="p-shadow-3">
-    <template slot="title">{{ workshop.type }} {{ date() }} {{ workshop.place.name }}</template>
-    <template slot="content">
-      <div>My Workshop Type is {{ workshop.type }}.</div>
-      <div>My id is {{ workshop.id }}.</div>
-      <div v-if="workshop.place.mapLink != ''">
-        I was held at
-        <a :href="workshop.place.mapLink">{{ workshop.place.name }}</a>.
-      </div>
-      <div v-else>I was held at {{ workshop.place.name }}</div>
-      <div>The date was  {{ date() }}.</div>
-      <div>I have been tagged with</div>
-      <div>
-        <li v-for="tag in workshop.tags" :key="tag">{{ tag }}</li>
-      </div>
-      <div>I got {{ workshop.upvotes }} upvotes.</div>
-    </template>
-    <template slot="footer">
-      <Button icon="pi pi-thumbs-up" label="Vote" />
-    </template>
-  </Card>
-  -->
   <VcABox :title="headline()" :expand="true" class="vca-blue-background vca-more-shadow workshop">
     <template slot="header">{{ date() }}</template>
     <div>I have been tagged with</div>
-      <div v-for="tag in workshop.tags" :key="tag">
-        <VcAFilterTag field="" :value="tag" />
-        </div>
+    <div class="p-grid tags">
+      <VcAFilterTag class="p-col" v-for="tag in workshop.tags" :key="tag" 
+        field="" 
+        :value="tag" 
+      />
+    </div>
     <div>I got {{ workshop.upvotes }} upvotes.</div>
   </VcABox>
 </template>
@@ -50,19 +30,13 @@ export default class WorkshopCard extends Vue {
 </script>
 
 <style scoped lang="less">
-.p-card {
-  background-color: lightgreen;
-  height: 100%;
-}
-.p-card /deep/ .p-card-body {
-  height: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: stretch;
-  flex-direction: column;
-}
 .workshop {
   height: 100%;
 }
+
+.tags {
+  margin: .5em;
+}
+
 
 </style>
