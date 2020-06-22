aaf162db1c3eadff877eb3f2e337fc463bff63a5
Tom Bierschenk <bierscht@hu-berlin.de>
2020-06-22 09:19:36 +0200

    remove unused import

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { VcAFrame, VcAColumn, VcABox, VcAInfoBox, VcAFilterTag } from 'vca-widget-base';
-import 'vca-widget-base/dist/vca-widget-base.css';
+import {
+  VcAFrame,
+  VcAColumn,
+  VcABox,
+  VcAInfoBox,
+  VcAFilterTag
+} from "vca-widget-base";
+import "vca-widget-base/dist/vca-widget-base.css";
 
 import "primevue/resources/themes/nova-vue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -12,15 +18,13 @@ import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
 import Button from "primevue/button";
-import Card from "primevue/card";
 
 Vue.component("Button", Button);
-Vue.component("Card", Card);
 Vue.component("VcAFrame", VcAFrame);
-Vue.component("VcAColumn",VcAColumn);
-Vue.component("VcABox",VcABox);
-Vue.component("VcAInfoBox",VcAInfoBox);
-Vue.component("VcAFilterTag",VcAFilterTag);
+Vue.component("VcAColumn", VcAColumn);
+Vue.component("VcABox", VcABox);
+Vue.component("VcAInfoBox", VcAInfoBox);
+Vue.component("VcAFilterTag", VcAFilterTag);
 
 Vue.config.productionTip = false;
 
